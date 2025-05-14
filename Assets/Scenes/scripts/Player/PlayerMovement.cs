8f8120b99294ccdc45248b8f82313a3c48ee5563
정재영 <wodud04277@gmail.com>
2025-05-14 13:11:46 +0900

    [Feat] Rotate Camera
    카메라 회전기능 구현

---
+++ Assets/Scenes/scripts/Player/PlayerMovement.cs
@@ -0,0 +1,98 @@
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private Transform _avatar;
+    [SerializeField] private Transform _aim;
+
+    private Rigidbody _rigidbody;
+    private PlayerStatus _playerStatus;
+
+    [Header("Mouse Config")]
+    [SerializeField][Range(-90, 0)] private float _minPitch;
+    [SerializeField][Range(0, 90)] private float _maxPitch;
+    [SerializeField][Range(0, 5)] private float _mouseSensitivity = 1;
+
+    private Vector2 _currentRotation;
+    private void Awake() => Init();
+    private void Init()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _playerStatus = GetComponent<PlayerStatus>();
+    }
+    public Vector3 SetMove(float moveSpeed)
+    {
+        Vector3 moveDirection = GetMoveDirection();
+
+        Vector3 velocity = _rigidbody.velocity;
+        velocity.x = moveDirection.x * moveSpeed;
+        velocity.z = moveDirection.z * moveSpeed;
+
+        _rigidbody.velocity = velocity;
+
+        return moveDirection;
+    }
+    public Vector3 SetAimRotation()
+    {
+        Vector2 mouseDir = GetMouseDirection();
+
+        //  x축의 경우라면 제한을 걸 필요 없음
+        _currentRotation.x += mouseDir.x;
+
+        Vector2 currentRotation = new()
+        {
+            x = transform.rotation.eulerAngles.x,
+            y = transform.rotation.eulerAngles.y
+        };
+
+        currentRotation.x += mouseDir.x;
+        currentRotation.y = Mathf.Clamp(
+            currentRotation.y + mouseDir.y,
+            _minPitch,
+            _maxPitch
+            );
+
+        // 캐릭터 오브젝트의 경우에는 Y축 회전만 반영
+        transform.rotation = Quaternion.Euler(0, _currentRotation.x, 0);
+
+        // 에임의 경우 상하 회전 반영
+        Vector3 currentEuler = _aim.localEulerAngles;
+        _aim.localEulerAngles = new Vector3(_currentRotation.y, currentEuler.y, currentEuler.z);
+
+        // 회전 방향 벡터 반환
+        Vector3 rotateDirVector = transform.forward;
+        rotateDirVector.y = 0;
+        return rotateDirVector.normalized;
+    }
+    public void SetBodyRotation()
+    {
+
+    }
+    private Vector2 GetMouseDirection()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * _mouseSensitivity;
+        float mouseY = -Input.GetAxis("Mouse Y") * _mouseSensitivity;
+
+        return new Vector2(mouseX, mouseY);
+    }
+    public Vector3 GetMoveDirection()
+    {
+        Vector3 input = GetInputDirection();
+
+        Vector3 direction =
+            (transform.right * input.x) +
+            (transform.forward * input.z);
+
+        return direction.normalized;
+    }
+    public Vector3 GetInputDirection()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+
+        return new Vector3(x, 0, z);
+    }
+}
